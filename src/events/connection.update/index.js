fbf479f3e241357e70b7429ca0049c6de80ee9e1
Kauãnzito <78958501+kauanzito@users.noreply.github.com>
2022-06-06 22:17:09 -0300

    Update index.js

--- src/events/connection.update/index.js
+++ src/events/connection.update/index.js
@@ -8,33 +8,33 @@ module.exports = () => sock.ev.on('connection.update', update => {
     if (connection == 'close') {
         let reason = new Boom(lastDisconnection?.error)?.output.statusCode;
         if (reason == DisconnectReason.badSession) {
-            console.log('arquivo de sessao invalido. Exclua a sessao e tente novamente.');
+            console.log('arquivo de sessão invalido. Exclua a sessão e tente novamente.');
             sock.logout();
         } else if (reason == DisconnectReason.connectionClosed) {
-            console.log('conexao fechada, reconectando...');
+            console.log('conexão fechada, reconectando...');
             fs.unlikSync('./auth.json'); process.exit();
         } else if (reason == DisconnectReason.connectionLost) {
-            console.log('conexao perdida dp servidor, reconectando...');
+            console.log('conexão perdida do servidor, reconectando...');
             fs.unlikSync('./auth.json'); process.exit();
         } else if (reason == DisconnectReason.connectionReplaced) {
-            console.log('conexao substituida, outra nova sessao aberta, fecha a sessao atual primeiro');
+            console.log('conexão substituída, outra nova sessao aberta, fecha a sessao atual primeiro');
             sock.logout();
             fs.unlinkSync('./auth.json');
         } else if (reason == DisconnectReason.loggedOut) {
-            console.log('Reinicializacao necessaria, reinciando...');
+            console.log('Reinicializacão necessária, reinciando...');
             fs.unlikSync('./auth.json'); process.exit();
         } else if (reason == DisconnectReason.timedOut) {
-            console.log('a conexao expirou, reconectando...');
+            console.log('a conexão expirou, reconectando...');
             fs.unlikSync('./auth.json'); process.exit();
         } else {
-            sock.end(`Razao de desconexao desconhecida: ${reason}|${connection}`);
+            sock.end(`Razão de desconexao desconhecida: ${reason}|${connection}`);
         };
 
         if (connection == 'connecting') {
             console.log('conectando...');
         }
         if (connection == 'connected') {
-            console.log('conectado');
+            console.log('conectado.');
         }
     }
 });
